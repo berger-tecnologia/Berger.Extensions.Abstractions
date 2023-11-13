3aab7369b0869314e608f698682dadefc0851bf7
Rafael Cronemberger <rafael@berger.com.br>
2023-11-13 14:18:59 -0300

    Package updates

---
+++ src/Berger.Extensions.Abstractions/Interfaces/Presentation/IBaseElement.cs
@@ -0,0 +1,19 @@
+﻿namespace Berger.Extensions.Abstractions
+{
+    public interface IBaseElement<T> where T : Enum
+    {
+        #region Properties
+        T ElementType { get; set; }
+        string Href { get; set; }
+        string Class { get; set; }
+        string Style { get; set; }
+        string Target { get; set; }
+        #endregion
+
+        #region Methods
+        void SetType(T type);
+        void SetHref(string link);
+        void SetClass(string classValue);
+        #endregion
+    }
+}
