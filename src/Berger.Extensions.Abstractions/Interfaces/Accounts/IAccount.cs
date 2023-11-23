67078f155b94f7970249b1e757bc2766afa00701
Rafael Cronemberger <rafael@berger.com.br>
2023-11-23 12:20:50 -0300

    Package updates

--- src/Berger.Extensions.Abstractions/Interfaces/Accounts/IAccount.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Berger.Extensions.Abstractions
-{
-    public interface IAccount
-    {
-        public void Logoff();
-        public string Signin(string user, string password);
-    }
-}
