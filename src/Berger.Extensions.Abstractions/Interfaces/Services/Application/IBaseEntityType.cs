6e38aecec7f197b9082e1423e2587ee5111cd3b3
Rafael Cronemberger <rafael@berger.com.br>
2024-02-08 10:43:08 -0300

    Package updates

---
+++ src/Berger.Extensions.Abstractions/Interfaces/Services/Application/IBaseEntityType.cs
@@ -0,0 +1,9 @@
+﻿namespace Berger.Extensions.Abstractions
+{
+    public interface IBaseEntityType<TSource, TDestination> : IBaseApplication<TSource, TDestination> where TSource : BaseEntity where TDestination : BaseEntity
+    {
+        #region Methods
+        List<TSource> GetByEntityTypeId(Guid id);
+        #endregion
+    }
+}
