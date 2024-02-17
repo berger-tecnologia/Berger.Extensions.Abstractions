cdaa71b1c3d04193e52aad5bfed0cbb5ebe9bebb
Rafael Cronemberger <rafael@berger.com.br>
2024-02-17 14:29:11 -0300

    Package updates

--- src/Berger.Extensions.Abstractions/Interfaces/Storage/IMedia.cs
+++ src/Berger.Extensions.Abstractions/Interfaces/Storage/IMedia.cs
@@ -3,7 +3,6 @@
     public interface IMedia
     {
         #region Properties
-        string Url { get; set; }
         string Alt { get; set; }
         bool Featured { get; set; }
         #endregion
