dc3457babdac37bd83bd6dec411d66ed71a09aa1
Rafael Cronemberger <rafael@berger.com.br>
2024-11-12 17:12:34 -0300

    Package updates

---
+++ src/Berger.Extensions.Abstractions/Interfaces/System/IContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Berger.Extensions.Abstractions
+{
+    public interface IContext
+    {
+        Guid ApplicationId { get; }
+        void SetApplication(Guid applicationId);
+    }
+    public interface IEntityContext
+    {
+        Guid EntityTypeId { get; }
+        Guid? EntityID { get; }
+        void SetEntityType(Guid entityTypeId, Guid? entityID);
+    }
+}
